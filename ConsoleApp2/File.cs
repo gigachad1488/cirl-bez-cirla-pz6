cbe9c6b22e611bf862d5dd5c0657d075095a7e82
student <student@B206-7>
2023-02-21 15:10:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class File
+    {
+        public string name { get; set; }
+
+        public DateTime date { get; set; }
+        public string type { get; set; }
+
+        public File(string name, string type, DateTime date)
+        {
+            this.name = name;
+            this.type = type;
+            this.date = date;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            List<File> files = new List<File>();
+            files.Add(new File("фай", ".exe", DateTime.Now));
+            files.Add(new File("adй", ".png", DateTime.Now));
+            List<string> fil = files.Where(s => s.type == ".exe").Select(s => s.name).ToList();
+
+            vivod(fil:fil.ToArray());
+            Console.ReadKey();
+        }
+
+        public static void vivod(int s = 0, params string[] fil)
+        {
+            if (s < fil.Length)
+            {
+                Console.WriteLine(fil[s]);
+                s++;
+                vivod(s, fil);
+            }
+        }
+
+        public static void ading(out List<File> files)
+        {
+            files = new List<File>();
+            string nam = Console.ReadLine();
+            string type = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            if (nam == "лив")
+            {
+                return;
+            }
+            files.Add(new File(nam, type, date));
+            ading(out files);
+        }
+    }
+}
